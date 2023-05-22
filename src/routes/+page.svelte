7531973794b8be9df2991b689ae14cb433b71312
Philippe Vinchon <p.vinchon@gmail.com>
2023-05-22 22:05:10 +0100

    Add basic coming soon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>NOK.IE | Just Knock</title>
+</svelte:head>
+
+<div
+	class="h-screen w-full grid content-center justify-center text-center bg-gradient-to-br from-[#FE6100] to-[#FE6100]/80"
+>
+	<div class="text-white">
+		<h1 class="text-[3em] sm:text-[4em] font-extrabold italic">COMING SOON</h1>
+		<p class="text-[1em] sm:text-[1.5em]">Website is under construction</p>
+	</div>
+</div>
