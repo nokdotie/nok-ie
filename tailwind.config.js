4601d2f990fd028d2efdb210bfff5805d49fa751
Philippe Vinchon <p.vinchon@gmail.com>
2023-05-26 22:53:38 +0100

    Move colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		colors: {
+			primary: '#FE6100',
+			neutral: {
+				800: '#222223',
+				700: '#616066',
+				600: '#8D8D91',
+				500: '#B7B7B7',
+				400: '#E9E9E9',
+				300: '#F6F6F6',
+				200: '#F9F9F9',
+				100: '#FFFFFF'
+			}
+		}
 	},
 	plugins: []
 };
