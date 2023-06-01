c93b594d251b7791125217242c89ea2774c93208
Philippe Vinchon <p.vinchon@gmail.com>
2023-06-01 11:11:22 +0100

    Reuse layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,12 +8,6 @@
 </svelte:head>
 
 <div
-	class="py-[20px] sm:py-[23px] md:py-[28px] lg:py-[32px] px-[16px] sm:px-[24px] mx-auto max-w-[1318px]"
->
-	<img src="/images/logo.svg" alt="NOK.IE Logo" class="h-[40px] md:h-[49px]" />
-</div>
-
-<div
 	class="pt-[40px] sm:pt-[50px] md:pt-[60px] lg:pt-[100px] pb-[100px] sm:pb-[125px] md:pb-[156px] lg:pb-[195px] items-center"
 >
 	<div
@@ -65,14 +59,5 @@
 				</p>
 			{/if}
 		</div>
-	</div>
-</div>
-
-<div class="py-[40px] sm:py-[44px] border-t-[1px] border-neutral-400">
-	<div
-		class="px-[16px] sm:px-[24px] mx-auto max-w-[1318px] flex items-center flex-col sm:flex-row sm:justify-between"
-	>
-		<img src="/images/logo.svg" alt="NOK.IE Logo" class="h-[49px] mb-[28px] sm:mb-0" />
-		<p class="text-paragraph-4 text-neutral-600">Copyright © NOK.IE</p>
 	</div>
 </div>
