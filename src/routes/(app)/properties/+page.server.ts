0479a5c4053f10541a53c8ff189efaa4f2830983
Philippe Vinchon <p.vinchon@gmail.com>
2023-06-21 17:32:57 +0100

    Unstyled form

--- src/routes/(app)/properties/+page.server.ts
+++ src/routes/(app)/properties/+page.server.ts
@@ -17,8 +17,8 @@ type QueryApiKeysResponse = {
 };
 
 const graphQlQuery = `
-    query($first: Int, $after: String) {
-        adverts(first: $first, after: $after) {
+    query($filter: AdvertsFilterInput, $first: Int, $after: String) {
+        adverts(filter: $filter, first: $first, after: $after) {
 			${connectionFragment(`
 				advertUrl
 				advertPriceInEur
@@ -33,8 +33,44 @@ const graphQlQuery = `
 `;
 
 export function load({ url }) {
-	const after = url.searchParams.get('after');
-	const variables = { first: 18, after: after };
+	function searchParamToNumber(key: string): number | null {
+		const value = url.searchParams.get(key);
+		const number = Number(value);
 
-	return query<QueryApiKeysResponse>(graphQlQuery, variables).then((response) => response.data);
+		return number === 0 && value !== '0' ? null : number;
+	}
+
+	const filter = {
+		address: { contains: url.searchParams.get('address.contains') },
+		priceInEur: {
+			greaterThanOrEqual: searchParamToNumber('priceInEur.greaterThanOrEqual'),
+			lessThanOrEqual: searchParamToNumber('priceInEur.lessThanOrEqual')
+		},
+		sizeInSqtMtr: {
+			greaterThanOrEqual: searchParamToNumber('sizeInSqtMtr.greaterThanOrEqual'),
+			lessThanOrEqual: searchParamToNumber('sizeInSqtMtr.lessThanOrEqual')
+		},
+		bedroomsCount: {
+			greaterThanOrEqual: searchParamToNumber('bedroomsCount.greaterThanOrEqual'),
+			lessThanOrEqual: searchParamToNumber('bedroomsCount.lessThanOrEqual')
+		},
+		bathroomsCount: {
+			greaterThanOrEqual: searchParamToNumber('bathroomsCount.greaterThanOrEqual'),
+			lessThanOrEqual: searchParamToNumber('bathroomsCount.lessThanOrEqual')
+		}
+	};
+	const after = url.searchParams.get('after');
+	url.searchParams.delete('after');
+
+	const variables = { first: 18, after: after, filter: filter };
+
+	return {
+		url: {
+			searchParams: url.searchParams.toString()
+		},
+		form: filter,
+		adverts: query<QueryApiKeysResponse>(graphQlQuery, variables).then(
+			(response) => response.data.adverts
+		)
+	};
 }
