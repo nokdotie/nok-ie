5907bb478847828d75b83af243011318f4a3284d
Philippe Vinchon <p.vinchon@gmail.com>
2023-06-26 20:40:30 +0100

    Add analytics

---
+++ src/lib/Analytics.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<script
+		async
+		src="https://analytics.umami.is/script.js"
+		data-website-id="25ff59c9-0e16-4b11-9092-1d48ad1cada9"
+		data-domains="nok.ie"
+	></script>
+</svelte:head>
