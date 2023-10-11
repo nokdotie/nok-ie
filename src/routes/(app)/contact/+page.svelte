c15ef7b4aef62d4525e0cd4a7130737d2dd95dab
Philippe Vinchon <p.vinchon@gmail.com>
2023-10-11 22:11:35 +0100

    Add contact page

---
+++ src/routes/(app)/contact/+page.svelte
@@ -0,0 +1,135 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	export let form;
+
+	const title = 'Nok.ie | Contact';
+	const description = "Questions? We've got answers. Reach out anytime!";
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+</svelte:head>
+
+<div class="px-6 py-24 sm:py-32 lg:px-8">
+	<div class="mx-auto max-w-xl">
+		<h2 class="text-4xl font-bold tracking-tight text-gray-900">Let’s talk</h2>
+		<p class="mt-2 text-lg leading-8 text-gray-600">
+			Questions? We've got answers. Reach out anytime!
+		</p>
+		<div class="mt-5">
+			<form method="POST" class="w-full" use:enhance>
+				<div class="grid grid-cols-1 gap-x-8 gap-y-6">
+					<div>
+						<label for="name" class="block text-sm font-semibold leading-6 text-gray-900"
+							>Name</label
+						>
+						<div class="mt-2.5">
+							<input
+								type="text"
+								name="name"
+								id="name"
+								autocomplete="given-name"
+								class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary focus:outline-none sm:text-sm sm:leading-6"
+							/>
+						</div>
+					</div>
+					<div>
+						<label for="email" class="block text-sm font-semibold leading-6 text-gray-900"
+							>Email</label
+						>
+						<div class="mt-2.5">
+							<input
+								id="email"
+								name="email"
+								type="email"
+								class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary focus:outline-none sm:text-sm sm:leading-6"
+							/>
+						</div>
+					</div>
+					<div class="sm">
+						<label for="message" class="block text-sm font-semibold leading-6 text-gray-900"
+							>Message</label
+						>
+						<div class="mt-2.5">
+							<textarea
+								id="message"
+								name="message"
+                                required
+								rows="4"
+								class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary focus:outline-none sm:text-sm sm:leading-6"
+							/>
+						</div>
+					</div>
+				</div>
+				<div class="mt-10">
+					<button
+						type="submit"
+						class="block w-full rounded-md bg-primary px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary disabled:bg-gray-300"
+                        disabled={form !== null}
+						>Send</button
+					>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+{#if form }
+    <div aria-live="assertive" class="fixed inset-0 flex items-end px-4 py-6 sm:p-6">
+    {#if form.ok === true}
+        <div class="rounded-md bg-green-50 p-4 w-full">
+            <div class="flex">
+                <div class="flex-shrink-0">
+                <svg class="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                </svg>
+                </div>
+                <div class="ml-3">
+                <p class="text-sm font-medium text-green-800">Successfully sent</p>
+                </div>
+                <div class="ml-auto pl-3">
+                <div class="-mx-1.5 -my-1.5">
+                    <button type="button" class="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50" on:click|preventDefault={() => form = null}>
+                    <span class="sr-only">Dismiss</span>
+                    <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+                    </svg>
+                    </button>
+                </div>
+                </div>
+            </div>
+        </div>
+    {:else}
+        <div class="rounded-md bg-red-50 p-4 w-full">
+            <div class="flex">
+                <div class="flex-shrink-0">
+                <svg class="h-5 w-5 text-red-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z" clip-rule="evenodd" />
+                </svg>
+                </div>
+                <div class="ml-3">
+                <p class="text-sm font-medium text-red-800">FSomething went wrong, try again in a few minutes</p>
+                </div>
+                <div class="ml-auto pl-3">
+                <div class="-mx-1.5 -my-1.5">
+                    <button type="button" class="inline-flex rounded-md bg-red-50 p-1.5 text-red-500 hover:bg-red-100 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 focus:ring-offset-red-50"  on:click|preventDefault={() => form = null}>
+                    <span class="sr-only">Dismiss</span>
+                    <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+                    </svg>
+                    </button>
+                </div>
+                </div>
+            </div>
+        </div>
+    {/if}
+    </div>
+{/if}
