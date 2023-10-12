4647b54793d1774e9b3b8d681fbcd7fac29b3e88
Philippe Hosey-Vinchon <p.vinchon@gmail.com>
2023-10-12 13:20:18 +0200

    Update link style to keep buttons but disable them (#34)

--- src/routes/(app)/properties/(list)/AdvertCardList.svelte
+++ src/routes/(app)/properties/(list)/AdvertCardList.svelte
@@ -40,21 +40,22 @@
 <div class="mx-auto w-full max-w-2xl px-4 py-8 sm:px-6 lg:max-w-7xl lg:px-8">
 	<nav aria-label="Pagination">
 		<div class="flex flex-1 justify-between sm:justify-end">
-			{#if $shouldShowPrevious}
-				<a
-					href="/"
-					on:click|preventDefault={BrowserHistoryBack}
-					class="relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0"
-					>Previous</a
-				>
-			{/if}
-			{#if connection.pageInfo.hasNextPage}
-				<a
-					href="{PropertiesRoute()}?{$urlSearchParams.toString()}"
-					class="relative ml-3 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0"
-					>Next</a
-				>
-			{/if}
+			<a
+				href="/"
+				on:click|preventDefault={BrowserHistoryBack}
+				class="relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0"
+				class:pointer-events-none={!$shouldShowPrevious}
+				class:bg-gray-50={!$shouldShowPrevious}
+				class:text-gray-500={!$shouldShowPrevious}
+				class:text-gray-900={$shouldShowPrevious}
+				class:ring-gray-200={!$shouldShowPrevious}>Previous</a
+			>
+			<a
+				href="{PropertiesRoute()}?{$urlSearchParams.toString()}"
+				class="relative inline-flex items-center rounded-md bg-primary hover:bg-primary/90 text-white px-3 py-2 text-sm font-semibold focus:outline-none ml-3"
+				class:pointer-events-none={!connection.pageInfo.hasNextPage}
+				class:bg-gray-300={!connection.pageInfo.hasNextPage}>Next</a
+			>
 		</div>
 	</nav>
 </div>
