a66ed893fe684122b656c320a76eb507ab9a278b
Philippe Hosey-Vinchon <p.vinchon@gmail.com>
2023-11-10 10:44:03 +0000

    Advertise (#40)
    * Rename property button to buy
    
    * Add new property page
    
    * Link to new property page
    
    * Sitemap new property page
    
    * Drop page size to 'encourage' browsing to next page

--- src/lib/layout/Header.svelte
+++ src/lib/layout/Header.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { writable } from 'svelte/store';
 	import PropertiesRoute from '../../routes/(app)/properties/(list)/Route';
+	import PropertiesNewRoute from '../../routes/(app)/properties/new/Route';
 
 	const mobileMenuOpen = writable(false);
 
 	const navigation = [
-		{ label: 'Properties', href: PropertiesRoute() }
+		{ label: 'Buy', href: PropertiesRoute() }
 		// { label: 'Search', href: '/search' },
 	];
 </script>
@@ -44,16 +45,29 @@
 				</svg>
 			</button>
 		</div>
-		<div class="hidden sm:flex sm:gap-x-12">
+		<div class="hidden sm:flex sm:gap-x-12 flex items-center">
 			{#each navigation as item}
 				<a href={item.href} class="text-sm font-semibold leading-6 text-gray-900">{item.label}</a>
 			{/each}
+			<a
+				href={PropertiesNewRoute()}
+				class="inline-flex items-center gap-x-1.5 rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary/90"
+			>
+				<svg class="-ml-0.5 h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+					<path
+						d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"
+					/>
+				</svg>
+				New Advert
+			</a>
 		</div>
 	</nav>
 
 	{#if $mobileMenuOpen}
 		<div class="sm:hidden" role="dialog" aria-modal="true">
-			<div class="fixed top-14 bottom-0 left-0 z-10 w-full overflow-y-auto bg-white px-6 py-6">
+			<div
+				class="fixed top-14 bottom-0 left-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 flex flex-col justify-between"
+			>
 				<div class="space-y-2">
 					{#each navigation as item}
 						<a
@@ -62,6 +76,20 @@
 							on:click={() => mobileMenuOpen.set(false)}>{item.label}</a
 						>
 					{/each}
+				</div>
+				<div class="space-y-2">
+					<a
+						href={PropertiesNewRoute()}
+						class="w-full inline-flex justify-center items-center gap-x-1.5 rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary/90"
+						on:click={() => mobileMenuOpen.set(false)}
+					>
+						<svg class="-ml-0.5 h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+							<path
+								d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"
+							/>
+						</svg>
+						New Advert
+					</a>
 				</div>
 			</div>
 		</div>
