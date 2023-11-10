f34ec1940b323f5bda296ab1713a0242303667f9
Philippe Vinchon <p.vinchon@gmail.com>
2023-11-10 09:19:06 +0000

    Add lastmod and changefreq to sitemap

--- src/lib/utils/SiteMapXml.ts
+++ src/lib/utils/SiteMapXml.ts
@@ -1,9 +1,17 @@
 function response(parentNode: string, childNode: string, urls: string[]) {
+	var lastmod = new Date().toISOString().slice(0, 10);
+
 	return new Response(
 		`
 		|<?xml version="1.0" encoding="UTF-8" ?>
 		|<${parentNode} xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-		|	${urls.map((url) => `<${childNode}><loc>${url}</loc></${childNode}>`).join('\n\t')}
+		|	${urls.map((url) => `
+		|		<${childNode}>
+		|			<loc>${url}</loc>
+		|			<lastmod>${lastmod}</lastmod>
+		|			<changefreq>daily</changefreq>
+		|		</${childNode}>
+		|	`).join('\n\t')}
 		|</${parentNode}>
         `
 			.trim()
