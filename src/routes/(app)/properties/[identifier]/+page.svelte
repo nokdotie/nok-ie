0e12dfb5e4065e7109d62dc02e7dfb9f0dde57c7
Philippe Vinchon <p.vinchon@gmail.com>
2023-12-26 12:28:45 +0000

    Display property not found message and encourage browsing

--- src/routes/(app)/properties/[identifier]/+page.svelte
+++ src/routes/(app)/properties/[identifier]/+page.svelte
@@ -1,75 +1,12 @@
 <script lang="ts">
-	import { onError } from '$lib/utils/HtmlImageElement';
-	import Gallery from './Gallery.svelte';
-	import Highlights from './Highlights.svelte';
-	import Seo from './Seo.svelte';
-	import Advertiser from './Advertiser.svelte';
+	import Property from './Property.svelte';
+	import PropertyNotFound from './PropertyNotFound.svelte';
 
 	export let data;
-
-	let tabs = ['Gallery'];
-
-	let activeTab = tabs[0];
 </script>
 
-<Seo advert={data.advert} />
-
-<div class="bg-white">
-	<div class="mx-auto px-4 sm:px-6 md:max-w-7xl md:px-8">
-		<div class="md:grid md:grid-cols-8 md:grid-rows-1 md:gap-x-8 md:gap-y-10 xl:gap-x-16">
-			<div class="md:col-span-5 md:row-end-1">
-				<div class="aspect-photo overflow-hidden rounded-lg bg-gray-100">
-					<img
-						src={data.advert.propertyImageUrls[0]}
-						on:error|preventDefault={onError('/images/placeholder.svg')}
-						alt=""
-						class="w-full h-full object-cover object-center"
-					/>
-				</div>
-			</div>
-
-			<div
-				class="mx-auto mt-14 max-w-2xl sm:mt-16 md:col-span-3 md:row-span-2 md:row-end-2 md:mt-0 md:w-full"
-			>
-				<h1 class="mt-4 text-2xl sm:text-3xl font-bold tracking-tight text-gray-900">
-					{data.advert.propertyAddress}
-				</h1>
-
-				<p class="mt-1 text-xl sm:text-2xl text-gray-500">
-					€{data.advert.advertPriceInEur.toLocaleString('en-IE')}
-				</p>
-
-				<Advertiser advert={data.advert} />
-				<Highlights advert={data.advert} />
-			</div>
-
-			<div class="mx-auto mt-16 w-full max-w-2xl md:col-span-5 md:mt-0 md:max-w-none">
-				<div>
-					<div class="border-b border-gray-200">
-						<div class="-mb-px flex space-x-8" aria-orientation="horizontal" role="tablist">
-							{#each tabs as tab}
-								<button
-									class="border-transparent {activeTab === tab
-										? 'text-primary  border-primary'
-										: 'text-gray-500 hover:text-gray-800 hover:border-gray-500'} whitespace-nowrap border-b-2 py-6 text-sm font-medium"
-									aria-controls="tab-{tab}"
-									role="tab"
-									type="button"
-									on:click={() => (activeTab = tab)}
-								>
-									{tab}
-								</button>
-							{/each}
-						</div>
-					</div>
-
-					{#if activeTab === 'Gallery'}
-						<div id="tab-panel-gallery" class="pt-10" aria-labelledby="tab-gallery" role="tabpanel">
-							<Gallery advert={data.advert} />
-						</div>
-					{/if}
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+{#if data.advert === null}
+	<PropertyNotFound />
+{:else}
+	<Property advert={data.advert} />
+{/if}
--- src/routes/(app)/properties/[identifier]/+page.ts
+++ src/routes/(app)/properties/[identifier]/+page.ts
@@ -4,10 +4,9 @@ import { graphQlQuery, type GraphQlQueryResponse } from './GraphQl';
 export function load({ params }) {
 	const identifier = params.identifier;
 	const variables = { identifier };
+	const advert = query<GraphQlQueryResponse>(graphQlQuery, variables)
+		.then((response) => response.data.advert)
+		.catch(() => null);
 
-	return {
-		advert: query<GraphQlQueryResponse>(graphQlQuery, variables).then(
-			(response) => response.data.advert
-		)
-	};
+	return { advert: advert };
 }
