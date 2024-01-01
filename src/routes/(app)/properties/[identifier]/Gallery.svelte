802a2df9263cfc98720d059f4c69560781bb7dee
Philippe Vinchon <p.vinchon@gmail.com>
2024-01-01 18:29:38 +0000

    Scale down image to remove the need to scroll

--- src/routes/(app)/properties/[identifier]/Gallery.svelte
+++ src/routes/(app)/properties/[identifier]/Gallery.svelte
@@ -56,65 +56,67 @@
 </ul>
 
 {#if null !== clickedIndex}
-	<div class="fixed inset-0 bg-black/90 backdrop-blur p-3 flex justify-center items-center">
-		<div class="max-w-full max-h-full overflow-y-scroll">
-			<div class="text-right">
-				<button on:click|preventDefault={() => (clickedIndex = null)}>
-					<svg
-						class="h-6 w-6 text-gray-300 hover:text-gray-100"
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M6 18L18 6M6 6l12 12"
-						/>
-					</svg>
-				</button>
-			</div>
-			<img src={advert.propertyImageUrls[clickedIndex]} alt="" />
-			<div class="flex justify-between group pt-3">
-				<button
-					on:click|preventDefault={clickPrevious}
-					class="inline-flex items-center p-2 text-sm font-medium text-gray-300 hover:text-gray-100"
+	<div class="fixed inset-0 bg-black/90 backdrop-blur p-2">
+		<div class="text-right">
+			<button on:click|preventDefault={() => (clickedIndex = null)}>
+				<svg
+					class="h-6 w-6 text-gray-300 hover:text-gray-100"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
 				>
-					<svg
-						class="mr-3 h-5 w-5 text-gray-300 group-hover:text-gray-100"
-						viewBox="0 0 20 20"
-						fill="currentColor"
-						aria-hidden="true"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M18 10a.75.75 0 01-.75.75H4.66l2.1 1.95a.75.75 0 11-1.02 1.1l-3.5-3.25a.75.75 0 010-1.1l3.5-3.25a.75.75 0 111.02 1.1l-2.1 1.95h12.59A.75.75 0 0118 10z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-					Previous
-				</button>
-				<button
-					on:click|preventDefault={clickNext}
-					class="inline-flex items-center p-2 text-sm font-medium text-gray-300 hover:text-gray-100 group"
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M6 18L18 6M6 6l12 12"
+					/>
+				</svg>
+			</button>
+		</div>
+		<img
+			src={advert.propertyImageUrls[clickedIndex]}
+			alt=""
+			class="m-auto object-scale-down h-[calc(100%-30px-44px)] w-full"
+		/>
+		<div class="flex justify-between group pt-2">
+			<button
+				on:click|preventDefault={clickPrevious}
+				class="inline-flex items-center p-2 text-sm font-medium text-gray-300 hover:text-gray-100"
+			>
+				<svg
+					class="mr-3 h-5 w-5 text-gray-300 group-hover:text-gray-100"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					aria-hidden="true"
 				>
-					Next
-					<svg
-						class="ml-3 h-5 w-5 text-gray-300 group-hover:text-gray-100"
-						viewBox="0 0 20 20"
-						fill="currentColor"
-						aria-hidden="true"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-				</button>
-			</div>
+					<path
+						fill-rule="evenodd"
+						d="M18 10a.75.75 0 01-.75.75H4.66l2.1 1.95a.75.75 0 11-1.02 1.1l-3.5-3.25a.75.75 0 010-1.1l3.5-3.25a.75.75 0 111.02 1.1l-2.1 1.95h12.59A.75.75 0 0118 10z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+				Previous
+			</button>
+			<button
+				on:click|preventDefault={clickNext}
+				class="inline-flex items-center p-2 text-sm font-medium text-gray-300 hover:text-gray-100 group"
+			>
+				Next
+				<svg
+					class="ml-3 h-5 w-5 text-gray-300 group-hover:text-gray-100"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					aria-hidden="true"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</button>
 		</div>
 	</div>
 {/if}
