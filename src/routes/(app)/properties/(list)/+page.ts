0d584442d6404d13fdcd9f81143dfbb8e007932b
Philippe Vinchon <p.vinchon@gmail.com>
2024-01-05 21:24:32 +0000

    Update GraphQL query

--- src/routes/(app)/properties/(list)/+page.ts
+++ src/routes/(app)/properties/(list)/+page.ts
@@ -1,18 +1,62 @@
 import { query } from '$lib/GraphQl';
+import { SearchFilters } from '../search/SearchFilters';
 import { graphQlQuery, type GraphQlQueryResponse } from './GraphQl';
 
-export function load({ url }) {
+export async function load({ url }) {
 	const after = url.searchParams.get('after');
-	const q = url.searchParams.get('q') || '';
+	const filters = SearchFilters.fromUrl(url);
+
+	let withinRectangle: null | {
+		northEast: { latitude: number; longitude: number };
+		southWest: { latitude: number; longitude: number };
+	};
+	if (
+		filters.locationNorthEastLat &&
+		filters.locationNorthEastLng &&
+		filters.locationSouthWestLat &&
+		filters.locationSouthWestLng
+	) {
+		withinRectangle = {
+			northEast: {
+				latitude: filters.locationNorthEastLat,
+				longitude: filters.locationNorthEastLng
+			},
+			southWest: {
+				latitude: filters.locationSouthWestLat,
+				longitude: filters.locationSouthWestLng
+			}
+		};
+	} else {
+		withinRectangle = null;
+	}
 
 	const variables = {
 		first: 24,
 		after: after,
-		filter: { address: { contains: q } }
+		filter: {
+			address: { contains: filters.q },
+			coordinates: { withinRectangle },
+			priceInEur: {
+				lessThanOrEqual: filters.priceInEurMax,
+				greaterThanOrEqual: filters.priceInEurMin
+			},
+			bedroomsCount: {
+				lessThanOrEqual: filters.bedroomsCountMax,
+				greaterThanOrEqual: filters.bedroomsCountMin
+			},
+			bathroomsCount: {
+				lessThanOrEqual: filters.bathroomsCountMax,
+				greaterThanOrEqual: filters.bathroomsCountMin
+			},
+			sizeInSqtMtr: {
+				lessThanOrEqual: filters.sizeInSqtMtrMax,
+				greaterThanOrEqual: filters.sizeInSqtMtrMin
+			}
+		}
 	};
 
 	return {
-		filters: { q },
+		filters,
 		adverts: query<GraphQlQueryResponse>(graphQlQuery, variables).then(
 			(response) => response.data.adverts
 		)
