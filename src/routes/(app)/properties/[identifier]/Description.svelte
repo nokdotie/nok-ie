80148a42013cd730d6c4b90371b5bde37dec9f2c
Sylwester Stocki <sylwester.stocki.rnd@gmail.com>
2024-01-05 16:16:43 +0000

    Adding property description

---
+++ src/routes/(app)/properties/[identifier]/Description.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let advert: {
+		propertyDescription: string;
+	};
+</script>
+
+{#if null !== advert.propertyDescription}
+	<div>
+		<h3 class="text-sm font-medium text-gray-900">Description</h3>
+		<p>{@html advert.propertyDescription.replace('\n', '<br>')}</p>
+	</div>
+{/if}
