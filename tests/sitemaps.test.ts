a1a1dafc20ca549f7587c6cc41828e5b922e1079
Philippe Vinchon <p.vinchon@gmail.com>
2024-03-03 12:44:15 +0000

    Sitemap tests to validate migration

---
+++ tests/sitemaps.test.ts
@@ -0,0 +1,19 @@
+import { expect, test, type Page } from '@playwright/test';
+
+const generic = async (page: Page, sitemapPath: string) => {
+	test.setTimeout(5 * 60 * 1000);
+
+	const response = await page.request.get(sitemapPath);
+	const siteMap = await response.text();
+	const locs = [...siteMap.matchAll(/<loc>(.*)<\/loc>/g)].map((match) => match[1]).slice(0, 5);
+
+	expect(locs.length).toBeGreaterThan(0);
+	for (const loc of locs) {
+		const response = await page.request.get(loc);
+		expect(response.status()).toBe(200);
+	}
+};
+
+test('/sitemap.xml contains working urls', async ({ page }) => generic(page, '/sitemap.xml'));
+test('/properties/sitemap.xml contains working urls', async ({ page }) =>
+	generic(page, '/properties/sitemap.xml'));
