654cdad0c93d07cfc0105c0983dd7c17b1d582f5
Philippe Vinchon <p.vinchon@gmail.com>
2024-03-13 11:51:48 +0000

    Add urls to sitemap and test those urls

--- src/routes/(seo)/sitemap_index.xml/+server.ts
+++ src/routes/(seo)/sitemap_index.xml/+server.ts
@@ -1,9 +1,11 @@
 import SiteMapXml from '$lib/components/seo/SiteMapXml';
 import PropertiesSiteMapRoute from '$routes/(app)/properties/sitemap.xml/Route';
+import PropertiesCountiesSiteMapRoute from '$routes/(app)/properties/counties/sitemap.xml/Route';
 
 export async function GET({ url }) {
 	return SiteMapXml.sitemapIndex([
 		`${url.origin}/sitemap.xml`,
-		`${url.origin}${PropertiesSiteMapRoute()}`
+		`${url.origin}${PropertiesSiteMapRoute()}`,
+		`${url.origin}${PropertiesCountiesSiteMapRoute()}`
 	]);
 }
