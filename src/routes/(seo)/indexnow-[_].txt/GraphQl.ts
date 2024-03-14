d97b9200cc56a28d798e4e2f55cd1297aebfe5d7
Philippe Hosey-Vinchon <p.vinchon@gmail.com>
2024-03-14 14:54:46 +0000

    Seo index now (#77)
    * Add index now endpoint
    
    * Validate index now key

---
+++ src/routes/(seo)/indexnow-[_].txt/GraphQl.ts
@@ -0,0 +1,19 @@
+export type GraphQlQueryResponse = {
+	data: {
+		seo: {
+			indexNow: {
+				isKeyValid: boolean;
+			};
+		};
+	};
+};
+
+export const graphQlQuery = `
+    query($key: String!) {
+        seo {
+            indexNow {
+                isKeyValid(value: $key)
+            }
+        }
+    }
+`;
