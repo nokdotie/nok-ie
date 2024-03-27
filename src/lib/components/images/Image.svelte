beb26708e2b98956d419f762b5feb6b3ab884205
Philippe Hosey-Vinchon <p.vinchon@gmail.com>
2024-03-27 12:40:44 +0000

    Hide bad image (#80)

--- src/lib/components/images/Image.svelte
+++ src/lib/components/images/Image.svelte
@@ -5,15 +5,19 @@
 	export let alt: string;
 
 	$: error = null === src || undefined === src || '' === src;
+
+	const copyrighted = [
+		'https://photos-a.propertyimages.ie/media/4/2/2/4775224/059d3b5b-db17-41fc-bc3c-92606d6c76c4_l.jpg'
+	].includes(src || '');
 </script>
 
-{#if !error}
-	<img {src} {alt} loading="lazy" class={$$props.class} on:error|once={() => (error = true)} />
-{:else}
+{#if error || copyrighted}
 	<div
 		class="{$$props.class} flex items-center justify-center gap-x-2.5 bg-neutral-700 text-neutral-100 text-lg font-medium leading-[1.111em]"
 	>
 		<CameraIcon class="w-[22px]" />
 		Image missing
 	</div>
+{:else}
+	<img {src} {alt} loading="lazy" class={$$props.class} on:error|once={() => (error = true)} />
 {/if}
